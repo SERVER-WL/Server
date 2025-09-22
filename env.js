5e78fdd72d1f274ee41f0c9db1891817283c58fe
SERVER-WL <novod0741@gmail.com>
2025-09-21 23:02:27 -0300

    Add files via upload

---
+++ env.js
@@ -0,0 +1,4 @@
+// env.js
+const bas = "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";
+
+export { bas };
